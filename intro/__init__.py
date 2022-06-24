d98fc2afa390d0fe604ed458483832362d2b7dc0
Dimitris Vasdekis <basdekd@gmail.com>
2022-06-24 23:25:53 +0300

    Update timeouts and threshold to production values
    Signed-off-by: Dimitris Vasdekis <basdekd@gmail.com>

--- intro/__init__.py
+++ intro/__init__.py
@@ -16,9 +16,9 @@ class C(BaseConstants):
     NUM_ROUNDS = 1
     NUM_OF_TABLES = 50
     # Dev threshold
-    TASK_THRESHOLD = 0
+    # TASK_THRESHOLD = 0
     # Real threshold
-    # TASK_THRESHOLD = 5
+    TASK_THRESHOLD = 5
     COMPREHENSION_QUESTION_BONUS = 0.10
 
     with open('tables.csv', encoding='utf-8-sig') as table_file:
@@ -200,7 +200,7 @@ class PracticeRoundIntro(Page):
 
 
 class PracticeRound(Page):
-    timeout_seconds = 10
+    timeout_seconds = 30
 
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
@@ -251,7 +251,7 @@ class RealTaskIntro(Page):
 
 
 class RealTask(Page):
-    timeout_seconds = 10
+    timeout_seconds = 120
 
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
@@ -370,7 +370,7 @@ class RedistributingIncomeFirst(Page):
 
 
 class MovingGroupSecond(Page):
-    timeout_seconds = 120
+    timeout_seconds = 180
 
     @staticmethod
     def is_displayed(player: Player):
@@ -386,7 +386,7 @@ class MovingGroupSecond(Page):
 
 
 class MovingGroupFirst(Page):
-    timeout_seconds = 120
+    timeout_seconds = 180
 
     @staticmethod
     def is_displayed(player: Player):
@@ -491,7 +491,7 @@ class QuestionIncomeAfterSwitching(Page):
     form_fields = ['question_income_after_switching']
 
 class QuestionIncomeAfterSwitchingResult(Page):
-    timeout_seconds = 60
+    timeout_seconds = 120
 
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
